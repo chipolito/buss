d2a9b0db7fc08b011e65b52a6d290be4a4994e43
Chipolito <chipolitoh@gmail.com>
2024-02-13 13:34:55 -0600

    migracion
    Actualizacion para mostrar motivos de viajes

--- src/models/motivo.model.js
+++ src/models/motivo.model.js
@@ -1,22 +1,41 @@
 'use strict'
 
-const db = require('./db');
+const { connectToDB, mSql }   = require('./db');
+const { logToFile } = require('../controllers/auxiliar.controller');
 
 class MotivoModel {
     Get(){
         return new Promise((resolve, reject) => {
-            let sqlCmd = `
-                SELECT 
-                    motivo_id,
-                    motivo_texto
-                FROM 
-                    motivo_viaje
-                WHERE
-                    motivo_estatus = 1;
-            `;
-            db.all(sqlCmd, [], (error, data) => {
-                let response = (error) ? { success: false, data: error, message: 'Error de base de datos' } : { success: true, data: data, message: 'Catalogo cargado correctamente' };
-                resolve(response);
+            connectToDB()
+            .then(async pool => {
+                try{
+                    const sqlCmd    = `
+                        SELECT 
+                            motivo_id,
+                            motivo_texto
+                        FROM 
+                            motivo_viaje
+                        WHERE
+                            motivo_estatus = 1;
+                    `;
+
+                    const result    = await pool
+                        .request()
+                        .query(sqlCmd);
+    
+                    resolve({ success: true, data: result.recordset, message: 'Catalogo cargado correctamente.' });
+                }
+                catch (error) {
+                    let strError = `motivo.model | Get | Error con la peticion al servidor de base de datos: ${JSON.stringify( error )}`;
+                    logToFile(strError, 'disse-tickets.log', '\r\n');
+                    resolve({success: false, data: error, message: 'Error con la peticion al servidor de base de datos.'});
+                } finally {
+                    pool.close()
+                }
+            })
+            .catch( error => {
+                logToFile('motivo.model | Get | ' + error, 'disse-tickets.log', '\r\n');
+                resolve({success: false, data: error, message: 'Error de servidor de base de datos.'});
             });
         });
     }
