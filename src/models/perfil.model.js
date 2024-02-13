608d999fd8bfd7ba3fee571e86afa8933129e011
Chipolito <chipolitoh@gmail.com>
2024-02-13 14:48:34 -0600

    migracion
    Actualizacion de modelo para perfil de usuario

--- src/models/perfil.model.js
+++ src/models/perfil.model.js
@@ -1,39 +1,72 @@
 'use strict'
 
-const db = require('./db');
+const { connectToDB, mSql }   = require('./db');
+const { logToFile } = require('../controllers/auxiliar.controller');
 
 class PerfilModel {
     Get( usuario_id ){
         return new Promise((resolve, reject) => {
-            let sqlCmd = `
-                SELECT u.usuario_contrasenia
-                FROM usuario As u
-                WHERE u.usuario_id = ?;
-            `;
+            connectToDB()
+            .then(async pool => {
+                try{
+                    const sqlCmd    = `
+                        SELECT u.usuario_contrasenia
+                        FROM usuario As u
+                        WHERE u.usuario_id = @usuarioId;
+                    `;
 
-            db.get(sqlCmd, usuario_id, (error, row) => {
-                let response = (error) ? {success: false, message: 'Error de base de datos'} : {success: true, data: row};
-                resolve(response);
+                    const result    = await pool
+                        .request()
+                        .input('usuarioId', mSql.Int, usuario_id)
+                        .query(sqlCmd);
+    
+                    resolve({ success: true, data: result.recordset.length > 0 ? result.recordset[0] : {} });
+                }
+                catch (error) {
+                    let strError = `usuario.model | Get | Error con la peticion al servidor de base de datos: ${JSON.stringify( error )}`;
+                    logToFile(strError, 'disse-tickets.log', '\r\n');
+                    resolve({success: false, message: 'Error con la peticion al servidor de base de datos.'});
+                } finally {
+                    pool.close()
+                }
+            })
+            .catch( error => {
+                logToFile('usuario.model | Get | ' + error, 'disse-tickets.log', '\r\n');
+                resolve({success: false, message: 'Error de servidor de base de datos.'});
             });
         });
     }
 
     PutPassword( data ) {
         return new Promise((resolve, reject) => {
-            let sqlCmd = `
-                UPDATE usuario 
-                    SET usuario_contrasenia = ?
-                WHERE usuario_id = ?
-            `;
+            connectToDB()
+            .then(async pool => {
+                try{
+                    const sqlCmd    = `
+                        UPDATE usuario 
+                            SET usuario_contrasenia = @contrasenia
+                        WHERE usuario_id = @usuarioId
+                    `;
+    
+                    const result    = await pool
+                        .request()
+                        .input('contrasenia', mSql.NVarChar, data.confirmPassword)
+                        .input('usuarioId', mSql.NVarChar, data.usuario_id)
+                        .query(sqlCmd);
 
-            let parameters = [
-                data.confirmPassword,
-                data.usuario_id
-            ];
-
-            db.run(sqlCmd, parameters, (error) => {
-                let response = (error) ? { success: false, data: error, message: 'Error de base de datos' } : { success: true, message: 'Contraseña actualizada correctamente' };
-                resolve(response);
+                    resolve({ success: true, message: 'Contraseña actualizada correctamente' });
+                }
+                catch (error) {
+                    let strError = `perfil.model | PutPassword | Error con la peticion al servidor de base de datos: ${JSON.stringify( error )}`;
+                    logToFile(strError, 'disse-tickets.log', '\r\n');
+                    resolve({success: false, data: error, message: 'Error con la peticion al servidor de base de datos.'});
+                } finally {
+                    pool.close()
+                }
+            })
+            .catch( error => {
+                logToFile('perfil.model | PutPassword | ' + error, 'disse-tickets.log', '\r\n');
+                resolve({success: false, data: error, message: 'Error de servidor de base de datos.'});
             });
         });
     }
